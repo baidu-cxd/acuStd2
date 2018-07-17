f057c0604b77ecdff1ea0aba1267984b4506b69a
dingzu <349923359@qq.com>
2018-07-17 21:10:50 +0800

    日常维护

---
+++ docs/.vuepress/components/line-height.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="line-height">
+    <div v-if="ask" v-for="i in ask" class="size-item"
+    :style="resourveStyle(i)">
+      字号: {{i}}  行高:{{resourveLineHeiht(i)}} <br> 对酒当歌，人生几何 <br> 譬如朝露，去日苦多
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+      small: {
+        type: Object,
+        default: function() {
+          return {
+          'fontSize': 12,
+          'lineHeiht': 24
+          }
+        }
+      },
+      big: {
+        type: Object,
+        default: function() {
+          return {
+          'fontSize': 42,
+          'lineHeiht': 64
+          }
+        }
+      },
+      ask: {
+        type: Array,
+        default: [12,48]
+      }
+    },
+    methods: {
+      resourveStyle(i) {
+        const h = this.resourveLineHeiht(i)
+        return "font-size:" + i + "px; line-height:" + h + "px;"
+      },
+      resourveLineHeiht(i) {
+        const a = this.small.fontSize
+        const b = this.small.lineHeiht
+        const c = this.big.fontSize
+        const d = this.big.lineHeiht
+        const e = (d - b) / (c - a) * (i - a) + b
+        const h = Math.floor(e)
+        return h
+      }
+    }
+}
+</script>
+
+<style lang="stylus">
+.line-height
+  .size-item
+    width 400px
+    border 1px solid #ebebeb
+    margin 40px 0
+    padding 0 10px
+</style>
