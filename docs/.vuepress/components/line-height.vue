0955097d7c17512ff5abeb0709d68b9ae2b1dce8
dingzu <349923359@qq.com>
2018-07-18 11:00:48 +0800

    添加sketch插件下载

--- docs/.vuepress/components/line-height.vue
+++ docs/.vuepress/components/line-height.vue
@@ -14,7 +14,7 @@ export default {
         default: function() {
           return {
           'fontSize': 12,
-          'lineHeiht': 24
+          'lineHeiht': 22
           }
         }
       },
